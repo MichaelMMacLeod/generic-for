500ed92a86a859a47c692cbe0b492149fb169c45
Michael MacLeod <michaelmmacleod@gmail.com>
2019-05-03 12:34:41 -0700

    Provide everything from private/

--- main.rkt
+++ main.rkt
@@ -8,8 +8,6 @@
          "private/from-list.rkt"
          "private/to-list.rkt")
 
-(provide (all-from-out "private/for.rkt"))
-
-(for (to-list #:reverse? #f)
-     ([(i) (from-list #:reverse? #t '(1 2 3 4 5))])
-  (+ i 2))
+(provide (all-from-out "private/for.rkt"
+                       "private/from-list.rkt"
+                       "private/to-list.rkt"))
