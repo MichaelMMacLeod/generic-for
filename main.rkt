7240a354e10c75f7a24b6d2be8c8443e73b431b8
Michael MacLeod <michaelmmacleod@gmail.com>
2019-05-04 12:45:48 -0700

    Remove old tests

--- main.rkt
+++ main.rkt
@@ -3,101 +3,11 @@
 (require racket/contract/base
          "private/accumulator.rkt"
          "private/iterator.rkt"
-         "private/for.rkt"
-         "private/list.rkt"
-         "private/vector.rkt"
-         "private/stream.rkt"
-         "private/void.rkt")
+         "private/for.rkt")
 
 (provide (all-from-out "private/accumulator.rkt"
                        "private/iterator.rkt"
-                       "private/for.rkt"
-                       "private/list.rkt"
-                       "private/vector.rkt"
-                       "private/stream.rkt"
-                       "private/void.rkt")
-         collection?
-         element?)
-
-(define collection? any/c)
-(define element? any/c)
+                       "private/for.rkt"))
 
 (module+ test
-  (require racket/port
-           rackunit)
-
-  (check-equal? (for ([x (from-list '(1 2 3 4 5))])
-                  x)
-                (for to-void
-                     ([x (from-list '(1 2 3 4 5))])
-                  x))
-
-  (check-equal? (for to-void
-                     ([x (from-list '(1 2 3 4 5))])
-                  x)
-                (for (to-void)
-                     ([x (from-list '(1 2 3 4 5))])
-                  x))
-
-  (check-equal? (for to-list
-                     ([x (from-list '(1 2 3 4 5))])
-                  x)
-                '(1 2 3 4 5))
-
-  (check-equal? (for (to-list #:reverse? #t)
-                     ([x (from-list '(1 2 3 4 5))])
-                  x)
-                '(5 4 3 2 1))
-
-  (check-equal? (for (to-vector #:length 10)
-                     ([i (from-list '(1 2 3 4 5))])
-                  i)
-                #(1 2 3 4 5 0 0 0 0 0))
-
-  (check-equal? (for (to-vector #:length 10
-                                #:fill #f)
-                     ([i (from-list '(1 2 3 4 5))])
-                  i)
-                #(1 2 3 4 5 #f #f #f #f #f))
-
-  (check-equal? (for (to-vector #:length 10
-                                #:proc values)
-                     ([i (from-list '(1 2 3 4 5))])
-                  i)
-                #(1 2 3 4 5 5 6 7 8 9))
-
-  (check-equal? (for (to-vector #:length 10
-                                #:proc (λ (x)
-                                         (* 2 x)))
-                     ([i (from-list '(1 2 3 4 5))])
-                  i)
-                #(1 2 3 4 5 10 12 14 16 18))
-
-  (check-equal? (for to-vector
-                     ([i (from-list '(1 2 3 4 5))])
-                  i)
-                #(1 2 3 4 5))
-
-  (check-equal? (for (to-vector #:grow-from 1)
-                     ([i (from-list '(1 2 3 4 5))])
-                  i)
-                #(1 2 3 4 5))
-
-  (check-equal? (for to-list
-                     ([i (from-stream (in-naturals))]
-                      [x (from-list '(a b c))])
-                  (cons i x))
-                '((0 . a) (1 . b) (2 . c)))
-
-  (check-equal? (for to-void
-                    ([i (from-list '(a b c))])
-                  i)
-                (void))
-
-  (check-equal? (with-output-to-string
-                  (λ ()
-                    (for (to-void #:proc write)
-                         ([i (from-list '(1 2 3))])
-                      (* 2 i))))
-                "246")
-  )
+  (require rackunit))
