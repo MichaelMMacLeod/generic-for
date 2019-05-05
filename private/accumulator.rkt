909d962e977128f88d1a63fcc0011e3296aa474d
Michael MacLeod <michaelmmacleod@gmail.com>
2019-05-05 00:43:12 -0700

    Remove commented-out code

--- private/accumulator.rkt
+++ private/accumulator.rkt
@@ -76,36 +76,6 @@
           (last-body ...)
           result))]))
 
-#;(define-syntax to-hash-set
-    (syntax-parser
-      [()
-       #'((last-body)
-          ([acc (hash)])
-          ((hash-set acc last-body #t))
-          acc)]))
-
-#;(define-syntax to-list
-    (syntax-parser
-      [()
-       (with-syntax ([(last-body) (generate-temporaries #'(tmp))])
-         #'((last-body)
-            ([acc null])
-            ((cons last-body acc))
-            (reverse acc)))]))
-
-#;(define-syntax to-fold
-    (syntax-parser
-      #:track-literals
-      [([fold-var:id start:expr]
-        ...
-        (~optional (~seq #:result result:expr)
-                   #:defaults ([result #'(values fold-var ...)])))
-       (with-syntax ([(last-body ...) (generate-temporaries #'((fold-var start) ...))])
-         #'((last-body ...)
-            ([fold-var start] ...)
-            (last-body ...)
-            result))]))
-
 #;(define-syntax to-void
     (syntax-parser
       [()
