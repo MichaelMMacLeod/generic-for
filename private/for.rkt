1085eeb468aa43e606a592103cd5f69c2a89d4c4
Michael MacLeod <michaelmmacleod@gmail.com>
2019-05-04 20:45:55 -0700

    Remove old unified-for macro and export the new one

--- private/for.rkt
+++ private/for.rkt
@@ -8,60 +8,10 @@
 
 (provide (rename-out [unified-for for]))
 
-(define-syntax (unified-for stx)
-  (syntax-parse stx
-    [(_ (~optional (~or (accumulator accumulator-args ...) accumulator)
-                   #:defaults ([accumulator #'to-void]))
-        ([var:id ...+ (iterator:id iterator-args:expr ...)] ...)
-        body ...)
-     (with-syntax
-       ([(accumulator-args ...)
-         (if (attribute accumulator-args)
-             #'(accumulator-args ...)
-             #'())])
-       (with-syntax
-         ([(((pre-bind ...)
-             (bind ...)
-             (test ...)
-             (post-bind ...)
-             (step ...))
-            ...)
-           (map (lambda (i i-args)
-                  (local-apply-transformer (syntax-local-value i)
-                                           i-args
-                                           'expression))
-                (syntax->list #'(iterator ...))
-                (syntax->list #'((var ... iterator-args ...) ...)))]
-          [((result ...) (a-bind ...) (a-insert ...) a-collect)
-           (local-apply-transformer (syntax-local-value #'accumulator)
-                                    (if #'(accumulator-args ...)
-                                        #'(accumulator-args ...)
-                                        #'())
-                                    'expression)])
-         #`(let* (pre-bind ... ...)
-             (let loop (bind ... ... a-bind ...)
-               (cond [(and test ... ...)
-                      (let-values ([(result ...)
-                                    (let (post-bind ... ...)
-                                      body ...)])
-                        (loop step ... ... a-insert ...))]
-                     [else a-collect])))))]))
-
-#;(let-values ([(i-outer-id ...) i-outer-expr] ...)
-    (let loop ([i-loop-id i-loop-expr] ...)
-      (if i-pos-guard
-          (let-values
-              ([(i-inner-id ...)
-                (match* (i-match-expr ...)
-                  [(@arg-patterns ...)
-                   @body ...])])
-            (loop i-loop-arg ...))
-          i-done-expr)))
-
 (define-for-syntax (apply-transformer transformer args)
   (local-apply-transformer (syntax-local-value transformer) args 'expression))
 
-(define-syntax (unified-for~ stx)
+(define-syntax (unified-for stx)
   (syntax-parse stx
     [(_ (accumulator:id accumulator-args:expr ...)
         ([pattern:expr ...+ (iterator:id iterator-args:expr ...)] ...)
@@ -117,41 +67,7 @@
         ((cons body-result acc))
         (reverse acc))]))
 
-(unified-for~ (to-list)
-              ([(cons a b) (from-vector #((1 . 2) (3 . 4) (5 . 6)))]
-               [x (from-vector #(1 2 3))])
-              (+ a b x))
-
-#;(let*-values ([(i-outer-id ...) i-outer-expr] ...)
-  (let loop ([a-loop-id a-loop-expr]
-             ...
-             [i-loop-id i-loop-expr]
-             ...)
-    (if (and i-pos-guard ...)
-        (let-values
-            ([(a-body-result ...)
-              (match (i-match-expr ...)
-                [(pattern ...)
-                 body ...])])
-          (loop i-loop-arg ...
-                a-loop-arg ...))
-        a-done-expr)))
-
-(let*-values ([(vect)                               ;         from-vector
-               #(1 2 3 4 5 6 7 8 9)]                ;         from-vector user
-              [(len)                                ;         from-vector
-               (vector-length vect)])               ;         from-vector
-  (let loop ([pos 0]                                ;         from-vector
-             [acc '()])                             ; to-list
-    (if (and (< pos len))                           ;         from-vector
-        (let-values                                 ;
-            ([(body-result)                         ; to-list
-              (match* ((vector-ref vect pos))       ;         from-vector
-                [((? number? n))                    ;                     user
-                 (define n*2                        ;                     user
-                   (* n 2))                         ;                     user
-                 (displayln n*2)                    ;                     user
-                 n*2])])                            ;                     user
-          (loop (add1 pos)                          ;         from-vector
-                (cons body-result acc)))            ; to-list
-        (reverse acc))))                            ; to-list
+(unified-for (to-list)
+             ([(cons a b) (from-vector #((1 . 2) (3 . 4) (5 . 6)))]
+              [x (from-vector #(1 2 3))])
+             (+ a b x))
