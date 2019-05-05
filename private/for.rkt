0e001bfe74e77633b1f77eabaa393274f22eb75c
Michael MacLeod <michaelmmacleod@gmail.com>
2019-05-04 20:43:29 -0700

    Allow for multiple iterator clauses

--- private/for.rkt
+++ private/for.rkt
@@ -64,36 +64,39 @@
 (define-syntax (unified-for~ stx)
   (syntax-parse stx
     [(_ (accumulator:id accumulator-args:expr ...)
-        ([pattern:expr ...+ (iterator:id iterator-args:expr ...)])
+        ([pattern:expr ...+ (iterator:id iterator-args:expr ...)] ...)
         body ...+)
      (define accumulator-result
        (apply-transformer #'accumulator #'(accumulator-args ...)))
-     (define iterator-result
-       (apply-transformer #'iterator #'(iterator-args ...)))
+     (define iterator-results
+       (map apply-transformer
+            (syntax->list #'(iterator ...))
+            (syntax->list #'((iterator-args ...) ...))))
      (syntax-parse accumulator-result
        [(([a-loop-id:id a-loop-expr:expr] ...)
          (a-body-result:id ...)
          (a-loop-arg ...)
          a-done-expr:expr)
-        (syntax-parse iterator-result
-          [(([(i-outer-id:id ...) i-outer-expr] ...)
-            ([i-loop-id:id i-loop-expr:expr] ...)
-            (i-pos-guard:expr ...)
-            (i-match-expr:expr ...)
-            (i-loop-arg ...))
-           #'(let*-values ([(i-outer-id ...) i-outer-expr] ...)
+        (syntax-parse iterator-results
+          [((([(i-outer-id:id ...) i-outer-expr] ...)
+             ([i-loop-id:id i-loop-expr:expr] ...)
+             (i-pos-guard:expr ...)
+             (i-match-expr:expr ...)
+             (i-loop-arg ...))
+            ...)
+           #'(let*-values ([(i-outer-id ...) i-outer-expr] ... ...)
                (let loop ([a-loop-id a-loop-expr]
                           ...
                           [i-loop-id i-loop-expr]
-                          ...)
-                 (if (and i-pos-guard ...)
+                          ... ...)
+                 (if (and i-pos-guard ... ...)
                      (let-values
                          ([(a-body-result ...)
-                           (match* (i-match-expr ...)
-                             [(pattern ...)
+                           (match* (i-match-expr ... ...)
+                             [(pattern ... ...)
                               body ...])])
                        (loop a-loop-arg ...
-                             i-loop-arg ...))
+                             i-loop-arg ... ...))
                      a-done-expr)))])])]))
 
 (define-syntax from-vector
@@ -115,11 +118,9 @@
         (reverse acc))]))
 
 (unified-for~ (to-list)
-              ([(? number? n) (from-vector #(1 2 3 4 5 6 7 8 9))])
-              (define n*2
-                (* n 2))
-              (displayln n*2)
-              n*2)
+              ([(cons a b) (from-vector #((1 . 2) (3 . 4) (5 . 6)))]
+               [x (from-vector #(1 2 3))])
+              (+ a b x))
 
 #;(let*-values ([(i-outer-id ...) i-outer-expr] ...)
   (let loop ([a-loop-id a-loop-expr]
