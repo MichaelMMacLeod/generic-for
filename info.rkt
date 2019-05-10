cd6240669764d0e01fea04de1d92d1475888cf84
Michael MacLeod <michaelmmacleod@gmail.com>
2019-05-10 13:37:05 -0700

    add expect to build-deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,13 @@
 #lang info
 (define collection "unified-for")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define deps 
+  '("base"))
+(define build-deps 
+  '("scribble-lib" 
+    "racket-doc" 
+    "rackunit-lib"
+    "expect"
+    ))
 (define scribblings '(("scribblings/unified-for.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
