fcbd93dac1556ae5d769f59a1d2b0516961a92ee
Michael MacLeod <michaelmmacleod@gmail.com>
2019-05-19 00:01:11 -0700

    Add sandbox-lib as a build-dep

--- info.rkt
+++ info.rkt
@@ -1,13 +1,13 @@
 #lang info
 (define collection "unified-for")
-(define deps 
+(define deps
   '("base"))
-(define build-deps 
-  '("scribble-lib" 
-    "racket-doc" 
+(define build-deps
+  '("scribble-lib"
+    "racket-doc"
     "rackunit-lib"
     "expect"
-    ))
+    "sandbox-lib"))
 (define scribblings '(("scribblings/unified-for.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
